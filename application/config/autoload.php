a30890dfe0c639cb9eaba36b04aeafa0a8e5e6f6
Muhammad Syafiq Fadillah <codingan77@gmail.com>
2026-07-22 14:41:39 +0700

    autoload datatable handler

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'email', 'session', 'datatables');
+$autoload['libraries'] = array('database', 'email', 'session', 'datatables', 'datatable_handler');
 
 /*
 | -------------------------------------------------------------------
