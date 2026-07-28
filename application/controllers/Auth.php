dc25b7e31a98e04ff30ec29ec9eb70eee4e2f80f
Muhammad Syafiq Fadillah <codingan77@gmail.com>
2026-07-28 15:35:31 +0700

    remove unneeded variabels in session auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -17,9 +17,6 @@
 
         public function login($bra, $id_grup, $email = null, $employee_initial = null)
         {
-            $user_email = "SELECT UsersId, Nama, EmployeeTypeId, Branches, Groups from db_fmm.dbo.kom_Users where Email = 'andy.ruswin@fajarmasmruni.com'";
-            $data_user = $this->db->query($user_email)->result_array();
-
             if ($bra == null || $bra == "") {
 
             } else {
@@ -27,11 +24,7 @@
                     'branch_id' => $bra,
                     'id_grup' => $id_grup,
                     'employee_initial' => $employee_initial,
-                    'id_sales' => $data_user[0]["UsersId"],
-                    'id_type_sales' => $data_user[0]["EmployeeTypeId"],
-                    'branch' => $data_user[0]["Branches"],
-                    'group' => $data_user[0]["Groups"],
-                    'nama' => $data_user[0]["Nama"],
+                    'email' => $email,
                     'logged_in' => true
                 );
 
